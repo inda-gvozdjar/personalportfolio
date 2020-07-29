313a6a11e02dcfb7f344f1398707f040ec96295d
Inda Gvozdjar <inda.gvozdjar@gmail.com>
2020-07-29 17:12:56 +0200

    fixing projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -5,14 +5,15 @@ class Projects extends Component {
     render() {
 
         if (this.props.data) {
-            var projects = this.props.data.map(function (projects) {
+            var description = this.props.data.description
+            var projects = this.props.data.projects.map(function (projects) {
                 var projectsimage = "images/" + projects.image;
                 var alt = 'picture of ' + projects.name
                 return (
                     <section id="projects">
                         <div className="row">
                             <div className="three columns">
-                                <img className="profile-pic" src={projectsimage} alt={alt} />
+                                <img className="projects-pic" src={projectsimage} alt={alt} />
                             </div>
                             <div className="nine columns main-col">
                                 <h2>{projects.name}</h2>
@@ -29,8 +30,8 @@ class Projects extends Component {
 
         return (
             <section id="projects">
+                <h4>{description}</h4>
                 {projects}
-                <h1>indaaa</h1>
             </section>
         );
     }
