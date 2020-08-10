44825e13c63924a9c4406d1558eec44bfda809ef
Inda Gvozdjar <inda.gvozdjar@gmail.com>
2020-08-10 03:02:16 +0200

    link mistake

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
 
   getResumeData() {
     $.ajax({
-      url: '/resumeData.json',
+      url: '/persnalportfolio/resumeData.json',
       dataType: 'json',
       cache: false,
       success: function (data) {
