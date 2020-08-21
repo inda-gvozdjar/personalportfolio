e03afe856c2d23ccb25a20d1d6f8dc153e5c9f0b
Inda Gvozdjar <inda.gvozdjar@gmail.com>
2020-08-21 11:30:50 +0200

    deleting contact

--- src/Components/About.js
+++ src/Components/About.js
@@ -29,19 +29,7 @@ class About extends Component {
 
                   <p>{bio}</p>
                   <Mucis />
-                  <div className="row">
-                     <div className="columns contact-details">
-                        <h2>Contact Details</h2>
-                        <p className="address">
-                           <span>{name}</span><br />
-                           <span>{street}<br />
-                              {city} {state}, {zip}
-                           </span><br />
-                           <span>{phone}</span><br />
-                           <span>{email}</span>
-                        </p>
-                     </div>
-                  </div>
+
                </div>
             </div>
 
